45192ac9cd66e4e6fdd7a7ac6210f5dd18d98df7
GuilhermeDias02 <guilherme.alex.dias@gmai.com>
2025-02-06 20:40:57 +0100

    jwt creation

--- movieBrooker/src/auth/auth.module.ts
+++ movieBrooker/src/auth/auth.module.ts
@@ -7,11 +7,15 @@ import { JwtAuthGuard } from './jwt-auth.guard';
 import { UserModule } from 'src/user/user.module';
 import { AuthService } from './auth.service';
 import { UserService } from 'src/user/user.service';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
     imports: [
         UserModule,
         PassportModule,
+        ConfigModule.forRoot({
+            envFilePath: '.env',
+        }),
         JwtModule.register({
             secret: process.env.JWT_SECRET,
             signOptions: { expiresIn: '7d' },
