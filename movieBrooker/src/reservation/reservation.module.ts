725a10c34ee1da441655fb8e6c4849dff937161b
GuilhermeDias02 <guilherme.alex.dias@gmai.com>
2025-02-07 17:11:38 +0100

    import orm

--- movieBrooker/src/reservation/reservation.module.ts
+++ movieBrooker/src/reservation/reservation.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { ReservationController } from './reservation.controller';
 import { ReservationService } from './reservation.service';
+import { Reservation } from './reservation.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  controllers: [ReservationController],
-  providers: [ReservationService]
+    imports: [
+        TypeOrmModule.forFeature([Reservation]),
+    ],
+    controllers: [ReservationController],
+    providers: [ReservationService]
 })
-export class ReservationModule {}
+export class ReservationModule { }
